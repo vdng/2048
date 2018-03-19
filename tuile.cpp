263ef7856bcc76eb869a433c2ff63c751a981f6d
theosantos <theo.santos@ecl16.ec-lyon.fr>
2018-03-19 11:41:34 +0100

    Modification de l'initialisation de Tuile

--- tuile.cpp
+++ tuile.cpp
@@ -1,13 +1,23 @@
 #include "tuile.h"
+#include <time.h>
 
 Tuile::Tuile(QObject *parent) : QObject(parent)
 {
-    valeur = 2;
-    couleur = 4c4545;
+    srand(time(NULL));
+    int rando = rand() % 100;
+    if rando>70{
+        valeur=4;
+    }
+    else{
+        valeur=2;
+    }
+    x=coord_x;
+    y=coord_y;
 }
 
 void Tuile::fusion(Tuile Tuile2){
-    valeur *= 2;
+    valeur*=2;
+    delete Tuile2;
 }
 
 void Tuile::get_val(){
