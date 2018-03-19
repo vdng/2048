735838d67f5721cbe6711164155f7fef74284620
theosantos <theo.santos@ecl16.ec-lyon.fr>
2018-03-19 10:35:17 +0100

    La même chose que précédemment mais en le faisant bien

--- tuile.h
+++ tuile.h
@@ -8,6 +8,21 @@ class Tuile : public QObject
     Q_OBJECT
 public:
     explicit Tuile(QObject *parent = nullptr);
+    void fusion(Tuile Tuile2);
+    int get_val();
+    void deplacer_gauche();
+    void deplacer_droite();
+    void deplacer_haut();
+    void deplacer_bas();
+    int get_x();
+    int get_y();
+
+private:
+    int valeur;
+    int couleur;
+    int x;
+    int y;
+
 
 signals:
 
