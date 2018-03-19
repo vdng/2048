23f2e6ee2d21b9b7409af6f041550511545c258e
theosantos <theo.santos@ecl16.ec-lyon.fr>
2018-03-19 10:30:40 +0100

    Création de la classe Tuile

---
+++ tuile.h
@@ -0,0 +1,17 @@
+#ifndef TUILE_H
+#define TUILE_H
+
+#include <QObject>
+
+class Tuile : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Tuile(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // TUILE_H
